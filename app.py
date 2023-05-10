af4bbd59fc5e9c3a2998c2ae82e06e2b5452a19c
Liliia Loboda <lilyaloboda2002@gmail.com>
2023-05-10 18:06:12 +0200

    Feat(New Devices Route): Create a new /set_device route to change a devices value

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, session, jsonify, render_template
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 
 @app.route('/')
-def load_dashboard():  # put application's code here
+def load_dashboard():
     return render_template("index.html")
+
+
+@app.route('/set_device', methods=['POST'])
+def set_device():
+    data = request.json
+    session["device_"+data['id']] = data['value'] == 'true'
+    print("Updated device_" + data['id'] + ": set to " + data['value'])
+    return jsonify(success=True)
 
 
 if __name__ == '__main__':
