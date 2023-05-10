047698a6786b3d6c52bd341d6107abb12ea38841
Liliia Loboda <lilyaloboda2002@gmail.com>
2023-05-10 22:10:03 +0200

    Feat(get_temperature): implement new get_temperature endpoint

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, request, session, jsonify, render_template
 
 app = Flask(__name__)
@@ -12,9 +14,16 @@ def load_dashboard():
 @app.route('/set_device', methods=['POST'])
 def set_device():
     data = request.json
-    session["device_"+data['id']] = data['value']
+    session["device_" + data['id']] = data['value']
     print("Updated device_" + data['id'] + ": set to " + data['value'])
     return jsonify(success=True)
+
+
+@app.route('/get_temperature')
+def get_temperature():
+    id = request.args.get('id')
+    temperature = round(random.uniform(20, 30), 1)
+    return jsonify({'id': id, 'temperature': temperature})
 
 
 if __name__ == '__main__':
